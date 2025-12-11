fe2ebdd4b4c4bfcf2682e3951db2a7de69144df0
Andrew Friedman <andrewfriedman01@gmail.com>
2025-12-10 22:01:17 -0500

    Add audio feedback to CoinPickup and SpecialCoinPickup scripts. Implemented coin sound playback with volume control and warnings for missing audio clips. Updated prefab files for both coin types.

--- Assets/Scripts/SpecialCoinPickup.cs
+++ Assets/Scripts/SpecialCoinPickup.cs
@@ -5,6 +5,18 @@ using System.Collections;
 public partial class SpecialCoinPickup : MonoBehaviour
 {
     public Transform coinEffect;
+    public AudioClip coinSound;
+    [Range(0f, 1f)]
+    public float coinVolume = 1f;
+    private const string CoinSoundWarning = "[SpecialCoinPickup] No coinSound assigned; assign a clip on the prefab.";
+
+    private void Awake()
+    {
+        if (!this.coinSound)
+        {
+            Debug.LogWarning(CoinSoundWarning, this);
+        }
+    }
     public virtual void OnTriggerEnter(Collider info)
     {
         if (info.name == "Ball")
@@ -12,8 +24,32 @@ public partial class SpecialCoinPickup : MonoBehaviour
             GameMaster.specialScore = GameMaster.specialScore + 1;
             Transform effect = UnityEngine.Object.Instantiate(this.coinEffect, this.transform.position, this.transform.rotation);
             UnityEngine.Object.Destroy(effect.gameObject, 3);
+
+            PlayCoinSound();
+
             UnityEngine.Object.Destroy(this.gameObject);
         }
+    }
+
+    private void PlayCoinSound()
+    {
+        if (!this.coinSound)
+        {
+            Debug.LogWarning(CoinSoundWarning, this);
+            return;
+        }
+
+        GameObject go = new GameObject("SpecialCoinPickupOneShot");
+        AudioSource src = go.AddComponent<AudioSource>();
+        src.clip = this.coinSound;
+        src.volume = this.coinVolume;
+        src.spatialBlend = 0f; // 2D so it’s always audible
+        src.dopplerLevel = 0f;
+        src.rolloffMode = AudioRolloffMode.Linear;
+        src.priority = 64;
+        src.Play();
+        Debug.Log("[SpecialCoinPickup] Played coin sound '" + this.coinSound.name + "' at volume " + this.coinVolume, this);
+        UnityEngine.Object.Destroy(go, this.coinSound.length);
     }
 
 }
